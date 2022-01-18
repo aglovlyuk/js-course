86cb96a1b18c109b383ed93f403274137f8ad913
aglovlyuk <aglovlyuk@gmail.com>
2022-01-18 17:17:10 +0200

    Test #1

---
+++ project-1/index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// task #1
+let userName = prompt('How is your name?');
+
+alert(userName);
+
+// task #2
+let a = 2;
+let b = 5;
+
+let sum = a + b;
+let subtraction = a - b;
+let multiplying = a * b;
+let division = a / b;
+let exponent = b ** a;
+
+alert(sum);
+alert(subtraction);
+alert(multiplying);
+alert(division);
+alert(exponent);
