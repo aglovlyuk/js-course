90c373409d029eda0591018287ef3e2bb894c2bd
Oleksii Hlovliuk <aglovlyuk@gmail.com>
2022-02-14 18:33:51 +0200

    HW #8 in progress

--- project-8/index.js
+++ project-8/index.js
@@ -1,5 +1,11 @@
 "use strict";
 
 // Task #1
+const myBirthsDate = new Date("1987-03-20");
+const myBirthsDay = myBirthsDate.getDate();
+const myBirthsMonth = ("0" + (myBirthsDate.getMonth() + 1)).slice(-2);
+const myBirthsYear = myBirthsDate.getFullYear();
+
+console.log(`${myBirthsDay}.${myBirthsMonth}.${myBirthsYear}`);
 
 // Task #2
