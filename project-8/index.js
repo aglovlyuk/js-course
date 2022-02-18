0ac4683104a5da8e0b644fd03107c06859d307d9
Oleksii Hlovliuk <aglovlyuk@gmail.com>
2022-02-18 17:04:11 +0200

    HW#8 fixes

--- project-8/index.js
+++ project-8/index.js
@@ -9,24 +9,25 @@
 // console.log(`${myBirthsDay}.${myBirthsMonth}.${myBirthsYear}`);
 
 // Task #2
-// const getDiffDays = function (firstDate, secondDate) {
-//     const startDate = Date.parse(firstDate);
-//     const endDate = Date.parse(secondDate);
-//     const msPerDay = 86400000;
+const getDiffDays = function (firstDate, secondDate) {
+    const startDate = new Date(firstDate);
+    const endDate = new Date(secondDate);
+    const MS_PER_DAY = 86400000;
+    const isValidDate = (date) => new Date(date).toString() !== "Invalid Date";
 
-//     if (isNaN(startDate) || isNaN(endDate)) {
-//         return "Error: invalid date";
-//     } else if (startDate > endDate) {
-//         return "Error: your start date is later than end";
-//     } else {
-//         return (endDate - startDate) / msPerDay;
-//     }
-// };
+    if (!isValidDate(startDate) || !isValidDate(endDate)) {
+        return "Error: invalid date";
+    } else if (startDate > endDate) {
+        return "Error: your start date is later than end";
+    } else {
+        return (endDate - startDate) / MS_PER_DAY;
+    }
+};
 
-// console.log(getDiffDays("2020-01-01", "2020-01-17")); // 16
-// console.log(getDiffDays("2020-01-01", "2020-03-15")); // 74
-// console.log(getDiffDays("2222222222", "2020-03-15")); // Error: invalid date
-// console.log(getDiffDays("2021-01-02", "2020-03-15")); // Error: your start date is later than end
+console.log(getDiffDays("2020-01-01", "2020-01-17")); // 16
+console.log(getDiffDays("2020-01-01", "2020-03-15")); // 74
+console.log(getDiffDays("2222222222", "2020-03-15")); // Error: invalid date
+console.log(getDiffDays("2021-01-02", "2020-03-15")); // Error: your start date is later than end
 
 // Task #3
 // const isWeekend = function (date) {
@@ -40,22 +41,22 @@
 // console.log(isWeekend("2022-02-09")); // false
 
 //Task #4
-const person = {
-    fullName: "Sherlock Holmes",
-    address: {
-        street: "Baker Street",
-        city: "London",
-        house: "221b",
-    },
-};
+// const person = {
+//     fullName: "Sherlock Holmes",
+//     address: {
+//         street: "Baker Street",
+//         city: "London",
+//         house: "221b",
+//     },
+// };
 
-const personsJSON = JSON.stringify(person);
-const personObj = JSON.parse(personsJSON);
-let {
-    fullName,
-    address: { street, city, house },
-} = person;
+// const personsJSON = JSON.stringify(person);
+// const personObj = JSON.parse(personsJSON);
+// let {
+//     fullName,
+//     address: { street, city, house },
+// } = person;
 
-console.log(personsJSON);
-console.log(personObj);
-console.log(fullName, street, city, house);
+// console.log(personsJSON);
+// console.log(personObj);
+// console.log(fullName, street, city, house);
