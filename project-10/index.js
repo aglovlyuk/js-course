98dc480bd7bc73301662180f69f9d5c81b3112da
Oleksii Hlovliuk <aglovlyuk@gmail.com>
2022-03-22 14:59:08 +0200

    Refactor folders

--- project-10/index.js
+++ project-10/index.js
@@ -1,29 +1,59 @@
 "use strict";
 
-const myTestPromise = new Promise(function (resolve, reject) {
-    setTimeout(() => {
-        //resolve("done");
-        reject(new Error("Error message"));
-    }, 5000);
-});
+const isWeekend = function (date) {
+    const dayOfWeek = new Date(date).getDay();
 
-myTestPromise
-    .then((result) => {
-        console.log(result);
-    })
-    .catch((error) => console.error(error))
-    .finally(() => console.log("Promise was finished!"));
+    return dayOfWeek === 0 || dayOfWeek === 6;
+};
 
-// async / await
-async function examplePromise() {
-    try {
-        const result = await myTestPromise;
-        console.log(result);
-    } catch (error) {
-        console.error(error);
-    } finally {
-        console.log("Promise was finished!");
+class Person {
+    constructor(firstName, lastName, age, birthDayDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.birthDayDate = birthDayDate;
+    }
+
+    celebrate() {
+        return "Happy Birthday, let’s celebrate";
     }
 }
 
-examplePromise();
+class Employee extends Person {
+    constructor(firstName, lastName, age, birthDayDate, jobPosition, salary) {
+        super(firstName, lastName, age, birthDayDate);
+        this.jobPosition = jobPosition;
+        this._salary = salary;
+    }
+
+    getYearSalary() {
+        return this._salary * 12;
+    }
+
+    celebrate() {
+        const birthdayThisYear = new Date(this.birthDayDate).setFullYear(
+            new Date().getFullYear()
+        );
+
+        return isWeekend(birthdayThisYear)
+            ? super.celebrate()
+            : "Happy Birthday, but I need to work";
+    }
+
+    get salary() {
+        return this._salary;
+    }
+}
+
+const person = new Person("Alex", "Smith", "35", "1987-03-20");
+const employee = new Employee(
+    "Kate",
+    "Smith",
+    "32",
+    "1989-12-06",
+    "Vue JS developer",
+    1000
+);
+
+console.log(person.celebrate());
+console.log(employee.celebrate());
--- project-11/index.js
+++ project-11/index.js
@@ -1,59 +1,29 @@
 "use strict";
 
-const isWeekend = function (date) {
-    const dayOfWeek = new Date(date).getDay();
+const myTestPromise = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        resolve("done");
+        reject(new Error("Error message"));
+    }, 5000);
+});
 
-    return dayOfWeek === 0 || dayOfWeek === 6;
-};
+myTestPromise
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((error) => console.error(error))
+    .finally(() => console.log("Promise was finished!"));
 
-class Person {
-    constructor(firstName, lastName, age, birthDayDate) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.birthDayDate = birthDayDate;
-    }
-
-    celebrate() {
-        return "Happy Birthday, let’s celebrate";
-    }
-}
-
-class Employee extends Person {
-    constructor(firstName, lastName, age, birthDayDate, jobPosition, salary) {
-        super(firstName, lastName, age, birthDayDate);
-        this.jobPosition = jobPosition;
-        this._salary = salary;
-    }
-
-    getYearSalary() {
-        return this._salary * 12;
-    }
-
-    celebrate() {
-        const birthdayThisYear = new Date(this.birthDayDate).setFullYear(
-            new Date().getFullYear()
-        );
-
-        return isWeekend(birthdayThisYear)
-            ? super.celebrate()
-            : "Happy Birthday, but I need to work";
-    }
-
-    get salary() {
-        return this._salary;
+// async / await
+async function examplePromise() {
+    try {
+        const result = await myTestPromise;
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    } finally {
+        console.log("Promise was finished!");
     }
 }
 
-const person = new Person("Alex", "Smith", "35", "1987-03-20");
-const employee = new Employee(
-    "Kate",
-    "Smith",
-    "32",
-    "1989-12-06",
-    "Vue JS developer",
-    1000
-);
-
-console.log(person.celebrate());
-console.log(employee.celebrate());
+examplePromise();
