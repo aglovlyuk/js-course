b11e46ccafe752e344026ad401a4d9ecf46af96e
Oleksii Hlovliuk <aglovlyuk@gmail.com>
2022-03-25 13:19:00 +0200

    HW #12

---
+++ project-12/index.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// then/catch method
+fetch("http://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((data) => {
+        const employeesOneCompany = data.filter(
+            (item) => item.company.name.toLowerCase() === "johns group"
+        );
+
+        console.log(employeesOneCompany);
+    })
+    .catch((error) => console.error(error));
+
+// async/await method
+async function getUsers() {
+    try {
+        const response = await fetch(
+            "http://jsonplaceholder.typicode.com/users"
+        );
+
+        if (!response.ok) {
+            throw response;
+        }
+
+        const data = await response.json();
+        const employeesOneCompany = data.filter(
+            (item) => item.company.name.toLowerCase() === "johns group"
+        );
+
+        console.log(employeesOneCompany);
+    } catch (responseError) {
+        console.error(error);
+    }
+}
+
+getUsers();
