c76ab5e3ffc94b0313c3992aedb89222499bd951
Oleksii Hlovliuk <aglovlyuk@gmail.com>
2022-04-06 14:43:37 +0300

    HW #15 done

--- project-14/index.js
+++ project-14/index.js
@@ -8,7 +8,23 @@ fetch("https://jsonplaceholder.typicode.com/albums")
             listItem.classList.add("album_title");
             listItem.innerHTML = item.title;
 
+            let removeBtn = document.createElement("button");
+            removeBtn.setAttribute("type", "button");
+            removeBtn.classList.add("btn-remove");
+            removeBtn.textContent = "X";
+            listItem.append(removeBtn);
+
             list.append(listItem);
+        });
+    })
+    .then(() => {
+        list.addEventListener("click", (event) => {
+            const isRemoveButton = event.target.className === "btn-remove";
+
+            if (isRemoveButton) {
+                const albumItem = event.target.closest(".album_title");
+                albumItem.remove();
+            }
         });
     })
     .catch((error) => console.error(error));
