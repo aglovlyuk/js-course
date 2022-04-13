4f87f94eeb6426d85abe6aa82a7c963965c136a7
Oleksii Hlovliuk <aglovlyuk@gmail.com>
2022-04-13 19:08:17 +0300

    HW #17 done

--- project-15/index.js
+++ project-15/index.js
@@ -1,40 +1,53 @@
 const list = document.getElementById("list");
 const form = document.forms.todo;
-const errorMessage = form.querySelector(".error-message");
+const errorMessage = form.querySelector(".invalid-feedback");
 const { todoinput } = form;
-const submitButton = form.querySelector(".todo-form__btn");
+const submitButton = form.querySelector("button[type=submit]");
+const template = document.getElementById("list-item");
 
 function isEmptyField(field) {
     return field.value.trim().length === 0;
 }
 
 const createElements = (text) => {
-    let listItem = document.createElement("li");
-    listItem.classList.add("todo-list__item");
-    listItem.innerHTML = text;
+    let listItem = template.content.cloneNode(true);
+    let textContainer = listItem.querySelector(".list-item-txt");
+    textContainer.textContent = text;
 
-    let removeBtn = document.createElement("button");
-    removeBtn.setAttribute("type", "button");
-    removeBtn.classList.add("btn-remove");
-    removeBtn.textContent = "X";
-    listItem.append(removeBtn);
+    // listItem.classList.add(
+    //     "list-group-item",
+    //     "d-flex",
+    //     "justify-content-between",
+    //     "align-items-center"
+    // );
+    // listItem.innerHTML = text;
+
+    // let removeBtn = document.createElement("button");
+    // removeBtn.setAttribute("type", "button");
+    // removeBtn.classList.add("btn-remove", "btn", "btn-danger");
+    // removeBtn.innerHTML = '<i class="bi bi-trash"></i>';
+    // listItem.append(removeBtn);
+
+    // let checkbox = document.createElement("input");
+    // checkbox.setAttribute("type", "checkbox");
+    // listItem.prepend(checkbox);
 
     list.append(listItem);
 };
 
 list.addEventListener("click", (event) => {
-    const isRemoveButton = event.target.className === "btn-remove";
+    const isRemoveButton = event.target.classList.contains("btn-remove");
 
     if (isRemoveButton) {
-        const albumItem = event.target.closest(".todo-list__item");
+        const albumItem = event.target.closest(".list-group-item");
+
         albumItem.remove();
     }
 });
 
 todoinput.oninput = () => {
     if (!isEmptyField(todoinput)) {
-        todoinput.classList.remove("error");
-        errorMessage.textContent = "";
+        todoinput.classList.remove("is-invalid");
     }
 };
 
@@ -42,8 +55,7 @@ form.onsubmit = (event) => {
     event.preventDefault();
 
     if (isEmptyField(todoinput)) {
-        todoinput.classList.add("error");
-        errorMessage.textContent = "Field cannot be empty";
+        todoinput.classList.add("is-invalid");
 
         return;
     }
@@ -52,3 +64,16 @@ form.onsubmit = (event) => {
 
     todoinput.value = "";
 };
+
+list.addEventListener("click", (event) => {
+    const targetElem = event.target;
+    const isCheckbox = targetElem.type === "checkbox";
+    const albumItem = targetElem.closest(".list-group-item");
+    const removeButton = albumItem.querySelector(".btn-remove");
+
+    if (isCheckbox && targetElem.checked) {
+        albumItem.classList.add("text-decoration-line-through");
+        removeButton.disabled = true;
+        targetElem.disabled = true;
+    }
+});
