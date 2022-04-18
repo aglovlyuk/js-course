64e22fce136dbb3196ed371c994132f8277690b5
Oleksii Hlovliuk <aglovlyuk@gmail.com>
2022-04-18 23:28:18 +0300

    HW #18 fix

--- project-18/index.js
+++ project-18/index.js
@@ -2,19 +2,42 @@ $(function () {
     const form = $(".figure-form");
     const { "select-figure": selectFigure, "color-range": colorRange } =
         form[0];
-    const figure = $("#figure");
+    const figureCanvas = $("#figure");
 
-    figure
-        .addClass(selectFigure.value)
-        .css("background-color", colorRange.value);
+    const figuresStyles = {
+        circle: {
+            width: "250px",
+            height: "250px",
+            borderRadius: "125px",
+        },
 
-    form.on("change", (event) => {
-        const targetElem = event.target;
+        rectangle: {
+            width: "250px",
+            height: "180px",
+            borderRadius: "5px",
+        },
 
-        if (targetElem.type === "color") {
-            figure.css("background-color", targetElem.value);
-        } else if (targetElem.tagName === "SELECT") {
-            figure.attr("class", selectFigure.value);
-        }
+        square: {
+            width: "250px",
+            height: "250px",
+            borderRadius: "0",
+        },
+    };
+
+    const createFigure = (figure, styles, animDuration = 400) => {
+        figure.animate(styles, animDuration, function () {
+            $(this).addClass("created");
+        });
+    };
+
+    createFigure(figureCanvas, figuresStyles[selectFigure.value], 0);
+    figureCanvas.css("background-color", colorRange.value);
+
+    $(colorRange).on("input", function () {
+        figureCanvas.css("background-color", this.value);
+    });
+
+    $(selectFigure).on("change", function () {
+        createFigure(figureCanvas, figuresStyles[this.value]);
     });
 });
