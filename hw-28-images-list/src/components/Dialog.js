64d06d3350a059aba2cc3a9b63a9e3f356c3bcf4
Oleksii Hlovliuk <aglovlyuk@gmail.com>
2022-06-27 00:51:04 +0300

    HW 29 90%

---
+++ hw-28-images-list/src/components/Dialog.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+
+const Popup = () => {
+    return console.log(11);
+};
+
+export default Popup;
--- hw-28-images-list/src/components/ImagesList.js
+++ hw-28-images-list/src/components/ImagesList.js
@@ -1,6 +1,16 @@
 import { useEffect, useState } from "react";
 
 import axios from "../helpers/axios";
+import Popup from "./Dialog";
+
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
 
 const ImagesList = () => {
     const [page, setPage] = useState(1);
@@ -12,7 +22,7 @@ const ImagesList = () => {
             .get("/list", {
                 params: {
                     page,
-                    limit: 10,
+                    limit: 5,
                 },
             })
             .then((nextImages) => {
@@ -25,15 +35,53 @@ const ImagesList = () => {
         setPage(page + 1);
     };
 
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const handleClickOpen = (item) => {
+        setSelectedImage(item);
+    };
+
+    const handleClose = () => {
+        setSelectedImage(null);
+    };
+
     return (
         <>
-            <ul className="list">
-                {images.map(({ id, download_url }) => (
-                    <li className="list__item" key={id}>
-                        <img className="list__img" src={download_url} alt="" />
-                    </li>
+            <ImageList sx={{ width: 500 }} cols={3} rowHeight={110}>
+                {images.map((item) => (
+                    //{ id, download_url, width, height, author }
+                    <ImageListItem key={item.id}>
+                        <img
+                            src={item.download_url}
+                            alt={item.author}
+                            loading="lazy"
+                            onClick={() => handleClickOpen(item)}
+                        />
+                    </ImageListItem>
                 ))}
-            </ul>
+            </ImageList>
+
+            <Dialog
+                open={selectedImage !== null}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogContent>
+                    {selectedImage && (
+                        <ul className="list__img-info">
+                            <li>Author: {selectedImage.author}</li>
+                            <li>Width: {selectedImage.width}</li>
+                            <li>Height: {selectedImage.height}</li>
+                        </ul>
+                    )}
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} autoFocus>
+                        Close
+                    </Button>
+                </DialogActions>
+            </Dialog>
 
             <button onClick={handleMoreImage} className="btn-primary">
                 {isLoading ? "Loading..." : "Load More"}
